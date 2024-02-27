0cf3bf1649101c5ab9f9560cd710ec8f2dbbd256
Simon Perkins <simon.perkins@gmail.com>
2024-02-27 13:53:11 +0200

    Clean up annotation code

--- predict/annotations.py
+++ predict/annotations.py
@@ -8,61 +8,8 @@ from dask.blockwise import Blockwise
 from typing import Iterable, Union
 from uuid import uuid4
 
-def is_key_arraylike(key):
-    return (isinstance(key, tuple) and
-            isinstance(key[0], str) and
-            all(isinstance(item, int) for item in key[1:]))
-
-def reduction_axes(layer):
-    try:
-        k, task = next(iter(layer.items()))
-    except StopIteration:
-        return False
-
-    if not is_key_arraylike(k):
-        return False
-
-    if not isinstance(task, tuple):
-        return False
-
-    fns = task[0].funcs if isinstance(task[0], Compose) else task[:1]
-
-    try:
-        pfn = next(iter(fn for fn in fns if isinstance(fn, partial)))
-    except StopIteration as e:
-        return False
-
-    try:
-        axis = pfn.keywords["axis"]
-        keepdims = pfn.keywords["keepdims"]
-    except KeyError as e:
-        return False
-    else:
-        return axis, keepdims
-
-
-def dummy_dimensions(ndims):
-    return tuple(f"dummy-{uuid4().hex[:8]}" for _ in range(ndims))
-
-
-def materialized_layer_dims(layer):
-    try:
-        k = next(iter(layer.keys()))
-    except StopIteration:
-        return False
-
-    # Needs to be array like
-    if not is_key_arraylike(k):
-        return False
-
-    try:
-        return layer.annotations["dims"]
-    except (KeyError, TypeError):
-        return dummy_dimensions(len(k[1:]))
-
 def annotate_datasets(datasets):
-    ds_chunks = [dict(ds.chunks) for ds in datasets]
-    ds_blocks = [{d: len(c) for d, c in chunks.items()} for chunks in ds_chunks]
+    ds_blocks = [{d: len(c) for d, c in ds.chunks.items()} for ds in datasets]
     row_blocks = [blocks["row"] for blocks in ds_blocks]
     total_row_blocks = sum(row_blocks)
     start_row_block = reduce(lambda a, v: a + [a[-1] + v], row_blocks, [0])
@@ -88,34 +35,82 @@ def annotate_datasets(datasets):
     return datasets
 
 
+def is_key_arraylike(key):
+    """Return True if key is a tuple of the form :code:`(str, int, ...)"""
+    return (isinstance(key, tuple) and
+            isinstance(key[0], str) and
+            all(isinstance(item, int) for item in key[1:]))
 
-def derive_dims_from_ancestors(layer, dep_layer_dims):
+def reduction_axes(layer):
+    """Return the axis/keepdims parameters of any reduction methods, or False """
+    try:
+        k, task = next(iter(layer.items()))
+    except StopIteration:
+        return False
+
+    # Sanity-checks
+    if not is_key_arraylike(k) or not isinstance(task, tuple):
+        return False
+
+    fns = task[0].funcs if isinstance(task[0], Compose) else task[:1]
+
+    try:
+        pfn = next(iter(fn for fn in fns if isinstance(fn, partial)))
+    except StopIteration as e:
+        return False
+
+    try:
+        return tuple(pfn.keywords[kw] for kw in ("axis", "keepdims"))
+    except KeyError as e:
+        return False
+
+
+def dummy_dimensions(ndims):
+    return tuple(f"dummy-{uuid4().hex[:8]}" for _ in range(ndims))
+
+
+def materialized_layer_dims(layer):
+    """ Get or generate dimensions of the materialized layer """
+    try:
+        # May already be present
+        return layer.annotations["dims"]
+    except (KeyError, TypeError):
+        # Generate dummy dimensions from the key length
+        try:
+            k = next(iter(layer.keys()))
+        except StopIteration:
+            return False
+
+        # Needs to be array like
+        if not is_key_arraylike(k):
+            return False
+
+        return dummy_dimensions(len(k[1:]))
+
+def dims_from_ancestors(layer, ancestor_dims):
+    """Returns a :code:`{i: d}` dict mapping layer indices to dimension names """
     index_dims = {}
 
-    for dep_layer_name, indices in layer.indices:
-        if indices is None:
+    for ancestor_name, indices in layer.indices:
+        if indices is None:  # Scalar?
             continue
 
+        # Get ancestor dimensions and assign them
+        # to the appropriate indices
         try:
-            dep_dims = dep_layer_dims[dep_layer_name]
+            dims = ancestor_dims[ancestor_name]
         except KeyError:
-            # Dependent layer name might be a wierd io_dep
-            try:
-                layer.io_deps[dep_layer_name]
-            except KeyError:
-                # Should be scalar, but complain if it isn't
-                if indices is not None:
-                    raise ValueError(
-                        f"{dep_layer_name} should be a scalar "
-                        f"but indices are {indices}")
-                continue
+            # References a dask.blockwise.BlockwiseDep,
+            # rather than an actual dask collection
+            # (i.e. a dask.array.Array)
+            if ancestor_name in layer.io_deps:
+                dims = dummy_dimensions(len(indices))
+            else:
+                raise KeyError(f"{ancestor_name} is not a valid dependency of {layer}")
+        if len(dims) != len(indices):
+            raise ValueError(f"Dimension lengths {dims} != {indices}")
 
-            dep_dims = dummy_dimensions(len(indices))
-
-        if len(dep_dims) != len(indices):
-            raise ValueError(f"Dimension lengths {dep_dims} != {indices}")
-
-        index_dims.update({i: d for i, d in zip(indices, dep_dims)})
+        index_dims.update(zip(indices, dims))
 
     return index_dims
 
@@ -132,19 +127,24 @@ def dim_propagator(dims: Union[str, Iterable[str]]):
         dataset_id = None
         ds_blocks = None
 
-        # Determine the dimension schema of dependent layers
-        # Make one up if one doesn't exist
+        # Obtain the dimension schema of ancestor layers
+        # Generate them if they don't exist
         for dep_layer_name in hlg.dependencies[x.name]:
             dep_layer = hlg.layers[dep_layer_name]
-            layer_annots = dep_layer.annotations or {}
 
             if isinstance(dep_layer, Blockwise):
                 ndims = len(dep_layer.output_indices)
             elif dep_layer.is_materialized():
                 k = next(iter(dep_layer.get_output_keys()))
-                assert isinstance(k, tuple) and isinstance(k[0], str)
+                assert is_key_arraylike(k)
                 ndims = len(k[1:])
+            else:
+                raise NotImplementedError(f"Layer type {dep_layer.layer_info_dict()['layer_type']}")
 
+            # Get any existing layer annotations on the ancestor layer
+            layer_annots = dep_layer.annotations or {}
+
+            # Get any dims on the ancestor, or generate some random dims
             try:
                 layer_dims = layer_annots["dims"]
             except (KeyError, TypeError):
@@ -152,27 +152,31 @@ def dim_propagator(dims: Union[str, Iterable[str]]):
 
             dep_layer_dims[dep_layer_name] = layer_dims
 
+            # Initialise dataset_id if not set,
+            # and ensure future matches once set
             if dataset_id is None:
                 dataset_id = layer_annots.get("dataset_id")
             else:
                 assert dataset_id == layer_annots.get("dataset_id", dataset_id)
 
+            # Initialise the dataset blocks if not set,
+            # and ensure future matches once set
             if ds_blocks is None:
                 ds_blocks = layer_annots.get("blocks")
             else:
                 assert ds_blocks == layer_annots.get("blocks", ds_blocks)
 
+        # Now, work out the dimensions of the current layer,
+        # possibly from the dependent layers
         if isinstance(layer, Blockwise):
-            # Infer output dimensions
-            index_dims = {}
-
             try:
-                # We may already have been provided with dimensions
+                # Defer to already existing annotations
                 output_dims = layer.annotations["dims"]
             except (KeyError, TypeError):
-                # Otherwise derice from dimensions provided on dependant layers
-                index_dims.update(derive_dims_from_ancestors(layer, dep_layer_dims))
+                # Map ancestor input dimensions to output dimensions
+                index_dims = dims_from_ancestors(layer, dep_layer_dims)
 
+                # Create dummy dimensions for any new axes
                 for new_axis in layer.new_axes:
                     index_dims[new_axis], = dummy_dimensions(1)
 
@@ -182,22 +186,26 @@ def dim_propagator(dims: Union[str, Iterable[str]]):
                     raise ValueError(f"Dimension lengths {layer_dims} != {layer.output_indices}")
         elif layer.is_materialized():
             if result := reduction_axes(layer):
-                # Reduction, the concatenation and aggregration layer are materialized graph
-                # Get the reduction axes and keepdim property
+                # Reduction case, the concatenation and aggregration layer
+                # are a materialized graph
+                # Obtain the reduction axes and keepdim property
                 axis, keepdims = result
                 dep_layers = hlg.dependencies[x.name]
                 assert len(dep_layers) == 1, dep_layers
                 dep_layer_name = next(iter(dep_layers))
                 output_dims = dep_layer_dims[dep_layer_name]
 
+                # Contraction, remove any dimensions
+                # not in the axis argument
                 if not keepdims:
                     output_dims = tuple(o for i, o in enumerate(output_dims) if i not in axis)
             elif result := materialized_layer_dims(layer):
+                # Materialized layer
                 output_dims = result
             else:
-                raise ValueError(f"Unhandled Layer type {layer.layer_info_dict()['layer_type']}")
+                raise NotImplementedError(f"Layer type {layer.layer_info_dict()['layer_type']}")
         else:
-            raise ValueError(f"Unhandled Layer type {layer.layer_info_dict()['layer_type']}")
+            raise NotImplementedError(f"Layer type {layer.layer_info_dict()['layer_type']}")
 
         out_annotations = {"dims": output_dims}
 
